c12a2e9ac59fee3b3c6d9038fc95847c3b82382c
Musti <rndm@capedfox.com>
2020-11-25 22:14:03 +0100

    Example App Configuration

--- example/src/app/app.component.spec.ts
+++ example/src/app/app.component.spec.ts
@@ -21,11 +21,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('example-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('example-app app is running!');
-  });
 });
